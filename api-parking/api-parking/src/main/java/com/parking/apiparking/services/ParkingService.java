6106cba49c8c82238a0f867bece573dd8b2a3450
DarkOvs77 <75710806+DarkOverlord689@users.noreply.github.com>
2023-04-16 21:34:14 -0500

    feat: Implementación remover auto y calcular tarifa de estacionamiento

--- api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
+++ api-parking/api-parking/src/main/java/com/parking/apiparking/services/ParkingService.java
@@ -3,13 +3,18 @@ package com.parking.apiparking.services;
 import com.parking.apiparking.entities.Car;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Service
 public class ParkingService {
 
     private List<Car> parkinglot;
+    private static final double HOURLY_RATE = 2.5;
 
     public ParkingService(){
         this.parkinglot = new ArrayList<>();
@@ -21,10 +26,40 @@ public class ParkingService {
     public List<Car> getAllCars(){
         return this.parkinglot;
     }
+
+    public Optional<Car> getCarByLicensePlate(String licensePlate){
+        return this.parkinglot.stream().filter(car -> car.getLicensePlate().equals(licensePlate)).findFirst();
+    }
+    public List<Car> getCarsByColor(String color){
+        return this.parkinglot.stream().filter(car -> car.getColor().equalsIgnoreCase(color)).collect(Collectors.toList());
+    }
     /*TODO: US 02: Como usuario, quiero poder agregar un auto al parqueo,
     para poder estacionar mi vehiculo.
      */
     public void addCar(Car car){
         this.parkinglot.add(car);
+    }
+
+    public boolean removeCarByLicensePlate(String licensePlate){
+        return this.parkinglot.removeIf(car -> car.getLicensePlate().equals(licensePlate));
+    }
+    public void parkCar(Car car){
+        car.setEntryTime(LocalDateTime.now());
+        addCar(car);
+    }
+    public void unpackCar(String licensePlate){
+        removeCarByLicensePlate(licensePlate);
+    }
+    public double calculateParkingFeee(String licensePlate){
+        Optional<Car> optionalCar = getCarByLicensePlate(licensePlate);
+        if(optionalCar.isPresent()){
+            Car car = optionalCar.get();
+            LocalDateTime entryTime = car.getEntryTime();
+            if(entryTime != null){
+                Long hoursParked = ChronoUnit.HOURS.between(entryTime, LocalDateTime.now());
+                return hoursParked * HOURLY_RATE;
+            }
+        }
+        return 0;
     }
 }
